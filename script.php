5597ba883d7f442cab1242d5aeef5e3a258de052
Tikha <64693040+Sub-Olahoop@users.noreply.github.com>
2020-06-22 23:29:05 +0800

    Create script.php

---
+++ script.php
@@ -0,0 +1,24 @@
+
+<script>
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '{794521401038177
+
+}',
+      cookie     : true,
+      xfbml      : true,
+      version    : '{EAALSnO3gFWEBABPK49FXbkfXNebZAHJZCDYxdNvxxAWlt9YhJ2MWc4q41HsaEzZA06a2WPtH7oG6ucZC5ErvcZCiTyEbSUJatPUpk4ZCUSbIZAZATZCQ8BDfTCu6vwCueDqCWrPIByV9I2bOYZCYtah0q37zNZBIplnhW5rRjiFDY4IZCqDU4Sr9yjntucFQcyf5idIZD}'
+    });
+      
+    FB.AppEvents.logPageView();   
+      
+  };
+
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "https://connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
+</script>
