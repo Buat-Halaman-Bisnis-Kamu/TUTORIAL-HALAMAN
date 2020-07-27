b3c65c5a61f045acb810989f0aa8f837e536176a
Tikha <64693040+Sub-Olahoop@users.noreply.github.com>
2020-07-27 19:34:10 +0700

    Update messenger_codes.php

--- Mesenjer/messenger_codes.php
+++ Mesenjer/messenger_codes.php
@@ -4,4 +4,4 @@ curl -X POST -H "Content-Type: application/json" -d '{
     "ref":"billboard-ad"
   },
   "image_size": 1000
-}' "https://graph.facebook.com/v2.6/me/messenger_codes?access_token=<ACCESS_TOKEN>"
+}' "https://graph.facebook.com/v2.6/me/messenger_codes?access_token=<EAALSnO3gFWEBAM0ZBN3T0QZAZCsRbF3xFRkiwsMcxYOYZBKmTNjPZCB4DGmrLt3VBbqpgOxu5qlfqUFkffIZBKlwmiWHBXd4Btk6bS2OHmoc5LXc5g0IM5zbZCkUSCLIZAzdCSN5u5wErghHShzr9NGQZA1eV2NSry6vKISe4meNChac3WFBzBjIg3ky5AkyADa8ZD>"
