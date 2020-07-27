4ec45efac75e627574f92daccfcd0d31333de6da
Tikha <64693040+Sub-Olahoop@users.noreply.github.com>
2020-07-27 18:32:48 +0700

    Create Mesenjer_webhook.php

---
+++ Code/Mesenjer_webhook.php
@@ -0,0 +1,14 @@
+{
+  "sender":{
+    "id":"<PSID>"
+  },
+  "recipient":{
+    "id":"<PAGE_ID>"
+  },
+  "timestamp":1458692752478,
+  "referral": {
+    "ref": "ref_data_in_m_dot_me_param",
+    "source": "SHORTLINK",
+    "type": "OPEN_THREAD",
+  }
+}
