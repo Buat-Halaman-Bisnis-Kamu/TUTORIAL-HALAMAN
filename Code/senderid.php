561087574590fe6ef71c716098ef349e7eb285b4
Tikha <64693040+Sub-Olahoop@users.noreply.github.com>
2020-07-27 18:12:37 +0700

    Create senderid.php

---
+++ Code/senderid.php
@@ -0,0 +1,18 @@
+
+{
+  "sender":{
+    "id":"<PSID>"
+  },
+  "recipient":{
+    "id":"<PAGE_ID>"
+  },
+  "timestamp":1458692752478,
+  "postback":{
+    "payload":"<USER_DEFINED_PAYLOAD>",
+    "referral": {
+      "ref": "ref_data_in_m_dot_me_param",
+      "source": "SHORTLINK",
+      "type": "OPEN_THREAD",
+    }
+  }
+}
