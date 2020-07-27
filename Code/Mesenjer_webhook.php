d77ea4bcbd438f9eb72ed3ab3b21869ab90d76e7
Tikha <64693040+Sub-Olahoop@users.noreply.github.com>
2020-07-27 18:46:18 +0700

    Update Mesenjer_webhook.php

--- Code/Mesenjer_webhook.php
+++ Code/Mesenjer_webhook.php
@@ -5,10 +5,10 @@
   "recipient":{
     "id":"<MDEwOkNoZWNrU3VpdGU5Njg2NjUwMjA>"
   },
-  "timestamp":1458692752478,
+  "timestamp":2020-07-27T11:42:17Z,
   "referral": {
     "ref": "ref_data_in_m_dot_me_param",
-    "source": "SHORTLINK",
+    "source": "https://github.com/Buat-Halaman-Bisnis-Kamu/TUTORIAL-HALAMAN/",
     "type": "OPEN_THREAD",
   }
 }
