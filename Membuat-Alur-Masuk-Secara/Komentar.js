871661e3e38d1c6ecc70fafef46d69d4125150d5
Tikha <64693040+Sub-Olahoop@users.noreply.github.com>
2020-07-28 08:20:49 +0700

    Create Komentar.js

---
+++ Membuat-Alur-Masuk-Secara/Komentar.js
@@ -0,0 +1,2 @@
+<div id="fb-root"></div>
+<script async defer crossorigin="anonymous" src="https://connect.facebook.net/id_ID/sdk.js#xfbml=1&version=v7.0&appId=572531576767979&autoLogAppEvents=1" nonce="zychpe9b"></script>
