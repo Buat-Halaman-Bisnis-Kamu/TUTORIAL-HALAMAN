299505cf3da87c17e376a1fff515c1c2187aaff5
Tikha <64693040+Sub-Olahoop@users.noreply.github.com>
2020-07-28 12:46:42 +0800

    Update Group.js

--- GroupFb/Group.js
+++ GroupFb/Group.js
@@ -24,7 +24,8 @@
          data-show-social-context="true" 
          data-show-metadata="false">
     </div>
-
+<div id="fb-root"></div>
+<script async defer crossorigin="anonymous" src="https://connect.facebook.net/id_ID/sdk.js#xfbml=1&version=v7.0&appId=701909577290417&autoLogAppEvents=1" nonce="vrWkvYXm"></script>
 </body>
 
 </html>
