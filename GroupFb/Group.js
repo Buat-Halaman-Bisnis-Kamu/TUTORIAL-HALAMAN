04bc63c69df20162d0d6d16a90d6f58a9e2e322a
Tikha <64693040+Sub-Olahoop@users.noreply.github.com>
2020-07-28 12:41:42 +0800

    Create Group.js

---
+++ GroupFb/Group.js
@@ -0,0 +1,30 @@
+<html>
+
+<head>
+    <!-- Load Facebook SDK for JavaScript -->
+    <div id="fb-root"></div>
+    <script>
+        (function(d, s, id) {
+            var js, fjs = d.getElementsByTagName(s)[0];
+            if (d.getElementById(id)) return;
+            js = d.createElement(s);
+            js.id = id;
+            js.src = 'https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.11&appId=<APP_ID>&autoLogAppEvents=1';
+            fjs.parentNode.insertBefore(js, fjs);
+        }(document, 'script', 'facebook-jssdk'));
+    </script>
+</head>
+
+<body>
+
+    <!--Your Group Plugin for the Web code-->
+    <div class="fb-group" 
+         data-href="<group_URL>" 
+         data-width="280" 
+         data-show-social-context="true" 
+         data-show-metadata="false">
+    </div>
+
+</body>
+
+</html>
